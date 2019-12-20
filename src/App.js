e9a4199f2375689f2268b1f17935b37bc0bcd52b
Gilbert <gssenyonjo@gmail.com>
2019-12-20 18:09:58 +0300

    Started up server. Learning react begins now

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+            React lessons begin
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
