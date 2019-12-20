4716b1596fdfde4774aa82f97ea465d2f1d0a6a3
Gilbert <gssenyonjo@gmail.com>
2019-12-20 23:43:58 +0300

    created clock timer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import React from 'react'
+import './App.css'
+
 
 function App() {
     return (
-        <h1>Hello React</h1>
+        <Clock/>
     )
 }
+
+class Clock extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { date: new Date() }
+    }
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        )
+    }
+    componentWillUnmount() {
+        clearInterval(this.timerID)
+    }
+    tick() {
+        this.setState({
+            date: new Date()
+        })
+    }
+    render() {
+        return (
+            <div>
+                <h1>The Great Timer</h1>
+                <h2>{ this.state.date.toLocaleTimeString() }</h2>
+            </div>
+        )
+    }
+}
+
 
 export default App
