d3ea3d67fcbc26ffc37043bfa2e49e7b9fc14d08
Gilbert <gssenyonjo@gmail.com>
2019-12-21 23:23:09 +0300

    FancyBorder and WelcomeDialog. This code was about composition in react. Next is a challenge from thinking in react. Its about building a searchable product data table

--- src/App.js
+++ src/App.js
@@ -3,111 +3,28 @@ import './App.css'
 
 function App() {
     return (
-      <Calculator />
+      <WelcomeDialog />
+    )
+}
+ 
+function FancyBorder(props) {
+    return (
+        <div className={'FancyBorder FancyBorder-' + props.color}>
+            {props.children}
+        </div>
     )
 }
 
-function BoilingVerdict(props) {
-    if(props.celsius >= 100) {
-        return <p>Water will boil</p>
-    }
-    return <p>Water will NOT boil</p>
-}
-
-const scaleNames = {
-    c: 'Celsius',
-    f: 'Fahrenheit'
-}
-
-function toCelsius(fahrenheit) {
-    return (parseFloat(fahrenheit) - 32) * 5 / 9;
-}
-
-function toFahrenheit(celsius) {
-    return (parseFloat(celsius) * 9 / 5) + 32;
-}
-
-
-class Calculator extends React.Component {
-    constructor(props) {
-        super(props)
-        this.handleCelsiusChange = this.handleCelsiusChange.bind(this)
-        this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this)
-        this.state = {
-            temperature: 27,
-            scale: 'c'
-        }
-    }
-
-    handleCelsiusChange(newTemperature) {
-        this.setState({
-            temperature: newTemperature,
-            scale: 'c'
-        })
-    }
-
-    handleFahrenheitChange(newTemperature) {
-        this.setState({
-            temperature: newTemperature,
-            scale: 'f'
-        })
-    }
-
-    render() {
-        const scale = this.state.scale
-        const temperature = this.state.temperature
-        let celsius
-        let fahrenheit
-        if(scale === 'c') {
-            celsius = temperature
-            fahrenheit = toFahrenheit(temperature)
-        } else if(scale === 'f') {
-            celsius = toCelsius(temperature)
-            fahrenheit = temperature
-        }
-        return (
-            <div>
-                <TemperatureInput 
-                    temperature = { celsius }
-                    scale='c'
-                    onTemperatureChange = { this.handleCelsiusChange } />
-                <TemperatureInput
-                    temperature = { fahrenheit }
-                    scale= 'f' 
-                    onTemperatureChange = { this.handleFahrenheitChange }/>
-            </div>
-        )
-    }
-}
-
-class TemperatureInput extends React.Component {
-    constructor(props) {
-        super(props)
-        this.handleChange = this.handleChange.bind(this)
-    
-    }
-    handleChange(e) {
-        this.props.onTemperatureChange(e.target.value)
-    }
-    render() {
-        let temperature 
-        let scale
-        if(this.props.scale === 'c') {
-            temperature = this.props.temperature
-            scale = this.props.scale
-        } else {
-            temperature = this.props.temperature
-            scale = this.props.scale
-        }
-        return (
-            <fieldset>
-                <legend>Enter temperature in { scaleNames[scale] }</legend>
-                <input
-                    value = { temperature }
-                    onChange = { this.handleChange } />
-                <BoilingVerdict celsius={parseFloat(temperature)}/>
-            </fieldset>
-        )
-    }
+function WelcomeDialog() {
+    return (
+        <FancyBorder color="blue">
+            <h1 className="Dialog-title">
+                Welcome to my dialog
+            </h1>
+            <p className="Dialog-message">
+                How have you been
+            </p>
+        </FancyBorder>
+    )
 }
 export default App
