634f1c60b5d6858de21f3bd26efe6bbf9be5ab71
Gilbert <gssenyonjo@gmail.com>
2019-12-22 10:58:25 +0300

    first hook example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import FilterableTable from './components/FilterableTable'
 
 function App() {
     return (
-      <FilterableTable />
+        <HookExample />
     )
 }
- 
+
+function HookExample() {
+    const [count, setCount] = useState(0)
+
+    return (
+        <div>
+            <p>You clicked { count } times</p>
+            <button onClick={ () => setCount(count + 1) }>
+                Click Me
+            </button>
+        </div>
+    )
+}
 
 export default App
