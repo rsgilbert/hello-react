378e09c1d87dee5bb705c4e2dccec840c886dab4
Gilbert <gssenyonjo@gmail.com>
2019-12-22 09:40:49 +0300

    created ul with list items of products. Search is working correctly

--- src/components/ProductTable.js
+++ src/components/ProductTable.js
@@ -1,14 +1,29 @@
 import React from 'react'
+import ProductItem from './ProductItem'
+
+const headingElement = <h3>Products we have</h3>
 
 class ProductTable extends React.Component {
     constructor(props) {
         super(props)
     }
     render() {
+        const searchText = this.props.searchText
+        const products = this.props.products.filter(
+            product => product.name.toLowerCase()
+                .includes(searchText)
+        )
         return (
             <div>
-                I am your ProductTable
-                {this.props.searchText}
+                { headingElement }
+                <ul>
+                    { products.map(
+                        product => 
+                        <ProductItem 
+                            product={product} 
+                            key={product.name}/>
+                    )}
+                </ul>                
             </div>
         )
     }
